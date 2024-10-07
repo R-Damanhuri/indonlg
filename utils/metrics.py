cbd802e020d7e588b6320511dc188b015402aba7
R. Damanhuri <96944447+R-Damanhuri@users.noreply.github.com>
2024-10-07 23:53:10 +0700

    change evaluate

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,13 +1,13 @@
 import itertools
 from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
 from .conlleval import conll_evaluation
-import datasets
+import evaluate
 from nltk import word_tokenize
 
-bleu = datasets.load_metric('bleu')
-rouge = datasets.load_metric('rouge')
-sacrebleu = datasets.load_metric('sacrebleu')
-squad_v2_metric = datasets.load_metric('squad_v2')
+bleu = evaluate.load('bleu')
+rouge = evaluate.load('rouge')
+sacrebleu = evaluate.load('sacrebleu')
+squad_v2_metric = evaluate.load('squad_v2')
 
 def emotion_detection_metrics_fn(list_hyp, list_label):
     metrics = {}
@@ -104,19 +104,19 @@ def news_categorization_metrics_fn(list_hyp, list_label):
 
 def generation_metrics_fn(list_hyp, list_label):
     # hyp and label are both list of string
-    list_hyp_bleu = list(map(lambda x: word_tokenize(x), list_hyp))
-    list_label_bleu = list(map(lambda x: [word_tokenize(x)], list_label))
-    list_label_sacrebleu = list(map(lambda x: [x], list_label))
+    list_hyp_bleu = list_hyp
+    list_label_bleu = [[label] for label in list_label]
+    list_label_sacrebleu = [[label] for label in list_label]
     
     metrics = {}
-    metrics["BLEU"] = bleu._compute(list_hyp_bleu, list_label_bleu)['bleu'] * 100
+    metrics["BLEU"] = bleu._compute(list_hyp_bleu, list_label_bleu, tokenizer=word_tokenize)['bleu'] * 100
     metrics["SacreBLEU"] = sacrebleu._compute(list_hyp, list_label_sacrebleu)['score']
     
     rouge_score = rouge._compute(list_hyp,list_label)
-    metrics["ROUGE1"] = rouge_score['rouge1'].mid.fmeasure * 100
-    metrics["ROUGE2"] = rouge_score['rouge2'].mid.fmeasure * 100
-    metrics["ROUGEL"] = rouge_score['rougeL'].mid.fmeasure * 100
-    metrics["ROUGELsum"] = rouge_score['rougeLsum'].mid.fmeasure * 100
+    metrics["ROUGE1"] = rouge_score['rouge1'] * 100
+    metrics["ROUGE2"] = rouge_score['rouge2'] * 100
+    metrics["ROUGEL"] = rouge_score['rougeL'] * 100
+    metrics["ROUGELsum"] = rouge_score['rougeLsum'] * 100
     return metrics
 
 def tydi_qa_metrics_fn(list_hyp, list_label):
